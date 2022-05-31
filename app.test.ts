89a6024606fec65d973fad10e6b78e9e82a67b2b
Ryo Uchino <rinfield@users.noreply.github.com>
2022-06-01 07:26:40 +0900

    add experimental module sample

--- app.test.ts
+++ app.test.ts
@@ -1,5 +1,5 @@
 import { assertSnapshot } from "https://deno.land/std@0.141.0/testing/snapshot.ts";
-import assertions from "https://esm.sh/aws-cdk-lib@2.26.0/assertions";
+import assertions from "https://esm.sh/aws-cdk-lib@2.26.0/assertions?deps=constructs@10.1.23";
 import { stack } from "./app.ts";
 
 Deno.test("snapshot", async (t) => {
--- app.ts
+++ app.ts
@@ -1,8 +1,18 @@
-import cdk from "https://esm.sh/aws-cdk-lib@2.26.0";
-import logs from "https://esm.sh/aws-cdk-lib@2.26.0/aws-logs";
+import cdk from "https://esm.sh/aws-cdk-lib@2.26.0?deps=constructs@10.1.23";
+import logs from "https://esm.sh/aws-cdk-lib@2.26.0/aws-logs?deps=constructs@10.1.23";
+import s3 from "https://esm.sh/aws-cdk-lib@2.26.0/aws-s3?deps=constructs@10.1.23";
+import firehose from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-alpha@2.26.0-alpha.0?deps=constructs@10.1.23";
+import firehoseDestinations from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-destinations-alpha@2.26.0-alpha.0?deps=constructs@10.1.23";
 
 const app = new cdk.App();
 export const stack = new cdk.Stack(app, "MyStack");
 new logs.LogGroup(stack, "MyLogGroup");
+
+const firehoseDestBucket = new s3.Bucket(stack, "FirehoseDestBucket", {});
+new firehose.DeliveryStream(stack, "FirehoseStream", {
+  destinations: [
+    new firehoseDestinations.S3Bucket(firehoseDestBucket),
+  ],
+});
 
 app.synth();
