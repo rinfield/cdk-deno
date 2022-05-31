041d855d23538e0e3eb403dae8f9a4495f9985ce
Ryo Uchino <rinfield@users.noreply.github.com>
2022-06-01 08:18:20 +0900

    Places dependent URLs into a local deps.ts file
    https://deno.land/manual/examples/manage_dependencies

--- app.test.ts
+++ app.test.ts
@@ -1,7 +1,6 @@
-import { assertSnapshot } from "https://deno.land/std@0.141.0/testing/snapshot.ts";
-import assertions from "https://esm.sh/aws-cdk-lib@2.26.0/assertions?deps=constructs@10.1.23";
+import { assertSnapshot, Template } from "./deps.ts";
 import { stack } from "./app.ts";
 
 Deno.test("snapshot", async (t) => {
-  await assertSnapshot(t, assertions.Template.fromStack(stack).toJSON());
+  await assertSnapshot(t, Template.fromStack(stack).toJSON());
 });
--- app.ts
+++ app.ts
@@ -1,8 +1,4 @@
-import cdk from "https://esm.sh/aws-cdk-lib@2.26.0?deps=constructs@10.1.23";
-import logs from "https://esm.sh/aws-cdk-lib@2.26.0/aws-logs?deps=constructs@10.1.23";
-import s3 from "https://esm.sh/aws-cdk-lib@2.26.0/aws-s3?deps=constructs@10.1.23";
-import firehose from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-alpha@2.26.0-alpha.0?deps=constructs@10.1.23";
-import firehoseDestinations from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-destinations-alpha@2.26.0-alpha.0?deps=constructs@10.1.23";
+import { cdk, firehose, firehoseTo, logs, s3 } from "./deps.ts";
 
 const app = new cdk.App();
 export const stack = new cdk.Stack(app, "MyStack");
@@ -11,7 +7,7 @@ new logs.LogGroup(stack, "MyLogGroup");
 const firehoseDestBucket = new s3.Bucket(stack, "FirehoseDestBucket", {});
 new firehose.DeliveryStream(stack, "FirehoseStream", {
   destinations: [
-    new firehoseDestinations.S3Bucket(firehoseDestBucket),
+    new firehoseTo.S3Bucket(firehoseDestBucket),
   ],
 });
 
