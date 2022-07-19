cd245a1851fca9a120a01cb34774aaffa0a6446a
Ryo Uchino <rinfield@users.noreply.github.com>
2022-07-19 17:32:02 +0900

    update dependencies

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-import awsCdkLib from "https://esm.sh/aws-cdk-lib@2.26.0?deps=constructs@10.1.23";
+import awsCdkLib from "https://esm.sh/aws-cdk-lib@2.32.1?deps=constructs@10.1.49";
 export const cdk = awsCdkLib;
 export const { aws_s3: s3, aws_logs: logs } = awsCdkLib;
 
-export { default as firehose } from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-alpha@2.26.0-alpha.0?deps=constructs@10.1.23";
-export { default as firehoseTo } from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-destinations-alpha@2.26.0-alpha.0?deps=constructs@10.1.23";
+export { default as firehose } from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-alpha@2.32.1-alpha.0?deps=constructs@10.1.49";
+export { default as firehoseTo } from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-destinations-alpha@2.32.1-alpha.0?deps=constructs@10.1.49";
 
 // test
-export { assertSnapshot } from "https://deno.land/std@0.141.0/testing/snapshot.ts";
+export { assertSnapshot } from "https://deno.land/std@0.148.0/testing/snapshot.ts";
 export const { Template } = awsCdkLib.assertions;
