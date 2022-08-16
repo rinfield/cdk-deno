40e0a2a12e0d4f5cec428f5580b0065fe80ea6fd
Ryo Uchino <rinfield@users.noreply.github.com>
2022-08-16 12:49:48 +0900

    use importmap

--- cdk-cli-deno/cdk.ts
+++ cdk-cli-deno/cdk.ts
@@ -1,4 +1,4 @@
 // https://github.com/aws/aws-cdk/blob/main/packages/aws-cdk/bin/cdk.ts
-import { cdkCliLib } from "../deps.ts";
+import cdkCliLib from "https://esm.sh/aws-cdk@2.30.0/lib?external=fsevents";
 
 cdkCliLib.cli();
--- deps.ts
+++ deps.ts
@@ -1,14 +1,12 @@
 // cdk-lib
-import awsCdkLib from "https://esm.sh/aws-cdk-lib@2.32.1?deps=constructs@10.1.49";
+import awsCdkLib from "aws-cdk-lib";
 export const cdk = awsCdkLib;
 export const { aws_s3: s3, aws_logs: logs } = awsCdkLib;
 
-export { default as firehose } from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-alpha@2.32.1-alpha.0?deps=constructs@10.1.49";
-export { default as firehoseTo } from "https://esm.sh/@aws-cdk/aws-kinesisfirehose-destinations-alpha@2.32.1-alpha.0?deps=constructs@10.1.49";
-
-// cdk-cli
-export { default as cdkCliLib } from "https://esm.sh/aws-cdk@2.32.1/lib";
+// TODO 最新のcdk-libを参照している？
+export { default as firehose } from "aws-kinesisfirehose-alpha";
+export { default as firehoseTo } from "aws-kinesisfirehose-destinations-alpha";
 
 // test
-export { assertSnapshot } from "https://deno.land/std@0.148.0/testing/snapshot.ts";
+export { assertSnapshot } from "https://deno.land/std@0.150.0/testing/snapshot.ts";
 export const { Template } = awsCdkLib.assertions;
