a163d54351dd6740c37bcd32b9e1e26d9a9455f4
Ryo Uchino <rinfield@users.noreply.github.com>
2022-11-17 11:44:03 +0900

    use deno npm url

--- cdk-cli-deno/cdk.ts
+++
@@ -1,4 +0,0 @@
-// https://github.com/aws/aws-cdk/blob/main/packages/aws-cdk/bin/cdk.ts
-import cdkCliLib from "https://esm.sh/aws-cdk@2.41.0/lib?external=fsevents";
-
-cdkCliLib.cli();
--- deps.ts
+++ deps.ts
@@ -1,9 +1,10 @@
 // cdk-lib
-import awsCdkLib from "aws-cdk-lib";
+import "constructs";
+import * as awsCdkLib from "aws-cdk-lib";
+
 export const cdk = awsCdkLib;
 export const { aws_s3: s3, aws_logs: logs } = awsCdkLib;
 
-// TODO 最新のcdk-libを参照している？
 export { default as firehose } from "aws-kinesisfirehose-alpha";
 export { default as firehoseTo } from "aws-kinesisfirehose-destinations-alpha";
 
