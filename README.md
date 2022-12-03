5af52f565b775808faeb881a99276e9d469c2b29
Ryo Uchino <rinfield@users.noreply.github.com>
2022-12-03 12:12:43 +0900

    Use npm specifier
    - removed node

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
-- `deno task ci` perform continuous integration checks
-- `cdk deploy` deploy this stack to your default AWS account/region
-- `cdk diff` compare deployed stack with current state
-- `cdk synth` emits the synthesized CloudFormation template
+- `deno task test` run tests using Deno's built-in test runner
+- `deno task ci` perform continuous integration checks(linter, formatter, tests)
+- `deno task cdk deploy` deploy this stack to your default AWS account/region
+- `deno task cdk diff` compare deployed stack with current state
+- `deno task cdk synth` emits the synthesized CloudFormation template
--- deps.ts
+++ deps.ts
@@ -1,13 +1,14 @@
 // cdk-lib
-import "constructs";
-import * as awsCdkLib from "aws-cdk-lib";
+import "npm:constructs@10.1.83";
+import * as awsCdkLib from "npm:aws-cdk-lib@2.50.0";
 
 export const cdk = awsCdkLib;
+
 export const { aws_s3: s3, aws_logs: logs } = awsCdkLib;
 
-export { default as firehose } from "aws-kinesisfirehose-alpha";
-export { default as firehoseTo } from "aws-kinesisfirehose-destinations-alpha";
+export { default as firehose } from "npm:@aws-cdk/aws-kinesisfirehose-alpha@2.50.0-alpha.0";
+export { default as firehoseTo } from "npm:@aws-cdk/aws-kinesisfirehose-destinations-alpha@2.50.0-alpha.0";
 
 // test
-export { assertSnapshot } from "testing/snapshot";
+export { assertSnapshot } from "https://deno.land/std@0.165.0/testing/snapshot.ts";
 export const { Template } = awsCdkLib.assertions;
