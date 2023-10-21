2a565b6218719f5689e3c4cccb4db7a6af741ac7
Ryo Uchino <rinfield@users.noreply.github.com>
2023-10-21 14:45:14 +0900

    `cdk deploy` / `cdk destroy` succeeds

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
 // cdk-lib
-import "npm:constructs@10.1.83";
-import * as awsCdkLib from "npm:aws-cdk-lib@2.50.0";
+import "constructs";
+import * as awsCdkLib from "aws-cdk-lib";
 
 export const cdk = awsCdkLib;
 
 export const { aws_s3: s3, aws_logs: logs } = awsCdkLib;
 
-export { default as firehose } from "npm:@aws-cdk/aws-kinesisfirehose-alpha@2.50.0-alpha.0";
-export { default as firehoseTo } from "npm:@aws-cdk/aws-kinesisfirehose-destinations-alpha@2.50.0-alpha.0";
+export { default as firehose } from "firehose";
+export { default as firehoseTo } from "firehoseTo";
 
 // test
-export { assertSnapshot } from "https://deno.land/std@0.165.0/testing/snapshot.ts";
+export { assertSnapshot } from "deno_std/testing/snapshot.ts";
 export const { Template } = awsCdkLib.assertions;
